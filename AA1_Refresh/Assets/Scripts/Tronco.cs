06f3fe1e914cd641ac91a592326abbb7cf1bde5f
jajo560 <joseantonio.jimenez@enti.cat>
2024-10-01 20:09:53 +0200

    rotationTurret

---
+++ AA1_Refresh/Assets/Scripts/Tronco.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Tronco : MonoBehaviour
+{
+    public GameObject troncoTorreta;
+    float pos_x = 0;
+    float pos_y = 0;
+
+        // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        pos_x = Input.GetAxis("Mouse X");
+        pos_y = Input.GetAxis("Mouse Y");
+        troncoTorreta.transform.localEulerAngles = transform.localEulerAngles + new Vector3(0, pos_x, 0);
+
+    }
+}
